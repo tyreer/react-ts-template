0fe31deb3bdec8b749af1f18de19776d9f3f0855
Robert T <20293876+tyreer@users.noreply.github.com>
2025-09-21 17:07:56 -0700

    fix: remove ThrowError setup validation code from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import Card from './components/Card';
 
-function ThrowError({ shouldThrow }: { shouldThrow: boolean }) {
-  if (shouldThrow) {
-    throw new Error('Test error message');
-  }
-  return <div>No error</div>;
-}
-
 function App() {
   return (
     <div>
       <Card>
-        <ThrowError shouldThrow={true} />
+        <h1>Hello World</h1>
       </Card>
     </div>
   );
